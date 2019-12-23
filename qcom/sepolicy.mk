2bd5f127dc1fb783ebc896ca952346cd6dfc9491
rezaadi0105 <rezaadipangestu5@gmail.com>
2019-12-24 02:58:24 +0700

    sepolicy: clean

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -9,14 +9,7 @@ TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
 endif
 endif
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/ancient/sepolicy/qcom/private
-
-ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/ancient/sepolicy/qcom/dynamic
-else
+ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/ancient/sepolicy/qcom/dynamic \
     device/ancient/sepolicy/qcom/vendor
 endif
