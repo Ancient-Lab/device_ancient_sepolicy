83cd3f4996a31aefb4f2db286b0c134e9164e457
rezaadi0105 <rezaadipangestu5@gmail.com>
2019-12-24 02:10:31 +0700

    sepolicy: ancient

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -10,16 +10,16 @@ endif
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/custom/sepolicy/common/public
+    device/ancient/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/custom/sepolicy/common/private
+    device/ancient/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/custom/sepolicy/common/dynamic
+    device/ancient/sepolicy/common/dynamic
 else
 BOARD_SEPOLICY_DIRS += \
-    device/custom/sepolicy/common/dynamic \
-    device/custom/sepolicy/common/vendor
+    device/ancient/sepolicy/common/dynamic \
+    device/ancient/sepolicy/common/vendor
 endif
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -10,13 +10,13 @@ endif
 endif
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/ancient/sepolicy/qcom/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/dynamic
+    device/ancient/sepolicy/qcom/dynamic
 else
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/dynamic \
-    device/lineage/sepolicy/qcom/vendor
+    device/ancient/sepolicy/qcom/dynamic \
+    device/ancient/sepolicy/qcom/vendor
 endif
